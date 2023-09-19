762425538428586ef61177ee26e8796548fff825
kkznch <kkznch710md@gmail.com>
2023-09-19 11:44:12 +0900

    add chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
+
+export default App;
